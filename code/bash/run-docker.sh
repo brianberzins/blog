59cc7d3b2a3bcf779d4a49730d8fbe652eecddf5
brianberzins <brianberzins@gmail.com>
2022-07-21 16:17:58 -0500

    rund command

---
+++ code/bash/run-docker.sh
@@ -0,0 +1,15 @@
+# Usage: rund <container-image>
+# Runs the specified container image as the current user, with the current directory mounted into the container.
+function rund() {
+  docker run \
+    --interactive \
+    --tty \
+    --user $(id -u):$(id -g) \
+    --workdir /home/$USER/$(basename "$PWD") \
+    --entrypoint /bin/bash \
+    --volume="/etc/group:/etc/group:ro" \
+    --volume="/etc/passwd:/etc/passwd:ro" \
+    --volume="/etc/shadow:/etc/shadow:ro" \
+    --volume "$PWD":/home/$USER/$(basename "$PWD") \
+    $1
+}
