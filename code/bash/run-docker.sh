c6fdeb5957479aa151b52fac0193cdec388d665d
brianberzins <brianberzins@gmail.com>
2022-07-22 13:32:30 -0500

    using a temp home directory

--- code/bash/run-docker.sh
+++ code/bash/run-docker.sh
@@ -1,18 +1,22 @@
 # Usage: rund <container-image>
 # Runs the specified container image as the current user, with the current directory mounted into the container.
 function rund() {
+  TEMP_HOME=$(mktemp --directory --quiet)
+  trap 'rm -rf "TEMP_HOME"' EXIT
+
   docker run \
     --interactive \
     --tty \
     --user $(id -u):$(id -g) \
-    --workdir /home/$USER/$(basename "$PWD") \
-    --entrypoint /bin/bash \
     --volume="/etc/group:/etc/group:ro" \
     --volume="/etc/passwd:/etc/passwd:ro" \
     --volume="/etc/shadow:/etc/shadow:ro" \
-    --volume="/home/$USER/.gitconfig:/home/$USER/.gitconfig" \
+    --volume="$TEMP_HOME:/home/$USER" \
     --volume="/home/$USER/.gitconfig:/home/$USER/.gitconfig" \
     --volume="/home/$USER/.ssh:/home/$USER/.ssh:ro" \
     --volume "$PWD":/home/$USER/$(basename "$PWD") \
-    $1
+    --workdir /home/$USER/$(basename "$PWD") \
+    --entrypoint /bin/bash \
+    "$@"
 }
+
