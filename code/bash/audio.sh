96d6edbd88306cb12454ccd82e5d0b04239e2be2
brianberzins <brianberzins@gmail.com>
2022-09-05 14:00:56 -0500

    audio setup

---
+++ code/bash/audio.sh
@@ -0,0 +1,3 @@
+pactl set-default-source alsa_output.usb-RODE_Microphones_RODE_Podcaster_v2_90DED076-00.iec958-stereo.monitor
+pactl set-default-sink alsa_output.pci-0000_01_00.1.hdmi-stereo-extra1
+
